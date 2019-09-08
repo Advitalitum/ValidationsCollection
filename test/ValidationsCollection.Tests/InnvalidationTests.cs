2eba2cee6ed23e51dcd8ade90bcf40b0153c83ae
D3N15T0R <for_letters_736@mail.ru>
2019-09-08 20:32:21 +0300

    Added Inn validations and tests

---
+++ test/ValidationsCollection.Tests/InnvalidationTests.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace ValidationsCollection.Tests
+{
+	public class InnValidationTests
+	{
+		[Theory]
+		[InlineData("7707083893")]
+		[InlineData("132808730606")]
+		[InlineData("500100732259")]
+		[InlineData("7830002293")]
+		public void IsInnValid_OnValidInnString_ReturnsTrue(string value) => ValidationsCollection.IsValidInn(value).Should().BeTrue();
+
+		[Theory]
+		[InlineData("7707083893 ")]
+		[InlineData("783¼002293")]
+		[InlineData("132808730602")]
+		[InlineData("")]
+		[InlineData("123")]
+		[InlineData("Hello")]
+		[InlineData((string) null)]
+		public void IsInnValid_OnInValidInnString_ReturnsTrue(string value) => ValidationsCollection.IsValidInn(value).Should().BeFalse();
+	}
+}
