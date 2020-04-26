cfb3976cbd5c185563fccc7b9bb0731a11cfbd80
Advitalitum <for_letters_736@mail.ru>
2020-04-26 11:45:33 +0300

    Added IsValidBik

---
+++ src/ValidationsCollection/BikValidations.cs
@@ -0,0 +1,51 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using JetBrains.Annotations;
+using static ValidationsCollection.ValidationsHelper;
+
+namespace ValidationsCollection
+{
+	public partial class Validations
+	{
+		/// <summary>
+		///     Determines whether the specified Bik string is valid
+		/// </summary>
+		/// <param name = "bikString"> The Bik string </param>
+		/// <returns>
+		///     <c> true </c> if is valid ; otherwise, <c> false </c>.
+		/// </returns>
+		/// <remarks>
+		///     Valid Bik string consists of 9 digits, where first digit equals "0", "1" or "2"
+		///     (https://ru.wikipedia.org/wiki/Банковский_идентификационный_код)
+		///     (http://www.consultant.ru/document/cons_doc_LAW_280683/24a8cab5291d2517c8fdaeee243a44901d717408/)
+		/// </remarks>
+		[Pure]
+		[PublicAPI]
+		[ContractAnnotation("null => false")]
+		public static bool IsValidBik([CanBeNull] [NotNullWhen(true)] string? bikString)
+		{
+			if (bikString is null || (bikString.Length != 9))
+			{
+				return false;
+			}
+
+			var first = GetNumericValue(bikString[0]);
+
+			if (first > 2)
+			{
+				return false;
+			}
+
+			for (var index = 1; index < 9; index++)
+			{
+				var current = GetNumericValue(bikString[index]);
+
+				if (current == NonDigitByte)
+				{
+					return false;
+				}
+			}
+
+			return true;
+		}
+	}
+}
---
+++ test/ValidationsCollection.Tests/BikValidationTests.cs
@@ -0,0 +1,36 @@
+﻿using FluentAssertions;
+using JetBrains.Annotations;
+using Xunit;
+
+namespace ValidationsCollection.Tests
+{
+	public class BikValidationTests
+	{
+		[Theory]
+		[InlineData("123123123")]
+		[InlineData("000000000")]
+		[InlineData("221321321")]
+		[InlineData("044525792")]
+		[InlineData("044525000")]
+		[InlineData("044525001")]
+		[InlineData("044525002")]
+		[InlineData("066666666")]
+		public void IsValidBik_OnValidBikString_ReturnsTrue(string value) =>
+			Validations.IsValidBik(value).Should().BeTrue();
+
+		[Theory]
+		[InlineData(null)]
+		[InlineData("")]
+		[InlineData("123")]
+		[InlineData("hello")]
+		[InlineData("123123 123")]
+		[InlineData("123123123 ")]
+		[InlineData("321321321")]
+		[InlineData("122AFF123")]
+		[InlineData("12312312¼")]
+		[InlineData("0000az000")]
+		[InlineData("744525000")]
+		public void IsValidBik_OnValidBikString_ReturnsFalse([CanBeNull] string? value) =>
+			Validations.IsValidBik(value).Should().BeFalse();
+	}
+}
